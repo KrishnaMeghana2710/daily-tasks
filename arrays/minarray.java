83d033b157e478e81227f4ac851560d7ecdec65c
meghana <krishnameghana2710@gmail.com>
2023-02-09 21:56:09 +0530

    first commit

---
+++ arrays/minarray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class minarray{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int a[]= new int[n];
+        for(int i=0; i<n; i++){
+            a[i]= sc.nextInt();
+        }
+        min(a);
+        sc.close();
+    }
+    static void min(int a[]){
+        int min= a[0];
+        for(int i=1; i<a.length; i++){
+            if(min>a[i]){
+                min= a[i];
+            }
+        }
+        System.out.println(min);
+    }
+
+}
