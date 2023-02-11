e844a0274aee6ed54bcac1abe2771b625bf4c2e5
meghana <krishnameghana2710@gmail.com>
2023-02-11 22:43:42 +0530

    Deleted class files

---
+++ Techademy Assignements/assignment2/MultiDimentionalArray.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class MultiDimentionalArray {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> list= new ArrayList<>();
+        
+    }
+    
+}
