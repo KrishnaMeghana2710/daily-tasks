6b469de63fe640fa667fb33ad7ad7ccb9cfa1260
meghana <krishnameghana2710@gmail.com>
2023-02-15 21:32:45 +0530

    spelling mistake

--- Techademy Assignements/assignment2/MultiDimentionalArray.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class MultiDimentionalArray {
-    public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>> list= new ArrayList<>();
-        
-    }
-    
-}
